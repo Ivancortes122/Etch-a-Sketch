66094c9b51bbf529cb3a69822772addb71945931
Ivan Cortes <ivancortes122@gmail.com>
2022-01-28 14:52:46 -0800

    updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 
+
 function createGrid(rows, columns) {
     let total = (rows * columns);
     for (let i = 0; i < total; i++) {
@@ -15,8 +16,28 @@ function mouseEnter () {
     Array.from(square).forEach((square) => {
         square.addEventListener('mousein', () => {
             square.classList.add('.hover');
-            console.log('hover');
         });
     });
 }
+
+const button = document.getElementById("clearBtn");
+
+button.addEventListener ('click', () => {
+    // alert(prompt("Select a number between 0 - 100"));
+    for (let i = total; i > 0; i--) {
+        container.removeChild(square);
+    }
+  
+
+    });
+
+
+
+
+
+
+
+
+
+
 
